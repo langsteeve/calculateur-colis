b58e8d6519a9b11f65ea3facfaa5b6c611cdd1c7
Teiki Lang <teikilang@Mac-Studio-de-Mac.local>
2025-04-05 23:31:25 -1000

    fix rendu PDF

--- src/App.js
+++ src/App.js
@@ -35,13 +35,19 @@ function App() {
 
   const exportPDF = () => {
     const element = document.getElementById('pdf-section');
-    html2canvas(element).then((canvas) => {
+  
+    html2canvas(element, { scale: 2 }).then((canvas) => {
       const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF();
-      pdf.addImage(imgData, 'PNG', 10, 10);
+      const pdf = new jsPDF('p', 'mm', 'a4');
+  
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+  
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
       pdf.save('colis-calcul.pdf');
     });
   };
+  
 
   return (
     <div className="App">
